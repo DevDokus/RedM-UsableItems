32669a0f8665642935614dbb21f80c71d9a7807a
DevDokus <49053928+DevDokus@users.noreply.github.com>
2021-03-16 12:03:02 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# RedM-UsableItems
+# RedM-UsableItems (WIP)
  Make custom items usable in RedM with ease!
+ 
+**Latest Version = 0.1.0**
+ 
+# Framework
+Currently this script only works with VORP. <br> 
+In the near future this script will also be <br> 
+compatible with RedEmRP. 
+
+# Features
+- Adding items with ease in the config
+- Adding or removing hunger
+- Adding or removing thirst
+- Adding or removing stamina
+- Adding or removing health
+
+# Note
+Remind that this code is still work in progress!         <br> 
+We advise not to change any of the code if you want      <br> 
+to be able to update to the next version flawlessly.     <br> 
+If you make changes to this script, you have to add      <br> 
+in the same changes once you update to a newer version.  <br> 
---
+++ config.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+----------------------------------- DevDokus -----------------------------------
+--------------------------------------------------------------------------------
+
+Items = {
+  {ID = 'consumable_coffee', Name = 'Coffee', Type = 'Eat', Hunger = -5, Thirst = 20, Stamina = 20, Health = 5},
+  {ID = 'consumable_peach', Name = 'Peach', Type = 'Eat', Hunger = 15, Thirst = 10, Stamina = 5, Health = 5},
+  {ID = 'tent', Name = 'Tent', Type = 'Placing', Hunger = 0, Thirst = 0, Stamina = -50, Health = 0},
+  {ID = 'campfire', Name = 'Campfire', Type = 'Placing', Hunger = 0, Thirst = 0, Stamina = -25, Health = 0},
+  {ID = 'consumable_sugarcube', Name = 'Sugercube', Type = 'Eat', Hunger = 5, Thirst = -5, Stamina = 5, Health = 5},
+  {ID = 'salmon', Name = 'Salmon', Type = 'Eat', Hunger = 10, Thirst = 15, Stamina = 10, Health = 15},
+  -- {ID = '', Name = '', Type = '', Hunger = 15, Thirst = 10, Stamina = 5, Health = 5},
+}
